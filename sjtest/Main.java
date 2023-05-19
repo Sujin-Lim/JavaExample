7549f2a4979242b5cb1fd937f19bbf888c37c983
sujin <lsj3369@gmail.com>
2023-05-19 15:43:33 +0900

    230519

--- sjtest/Main.java
+++
@@ -1,9 +0,0 @@
-package sjtest;
-
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-        }
-    }
--- sjtest/pyramid.java
+++
@@ -1,9 +0,0 @@
-package sjtest;
-
-public class pyramid {
-    public static void main(String[] args) {
-        System.out.println("  *  ");
-        System.out.println(" *** ");
-        System.out.println("*****");
-    }
-}
