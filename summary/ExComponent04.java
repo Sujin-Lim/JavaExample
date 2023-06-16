e3f3133b81e736332a5774212832880987675046
sujin <lsj3369@gmail.com>
2023-06-16 11:09:33 +0900

    230616

--- summary/ExComponent04.java
+++ summary/ExComponent04.java
@@ -13,9 +13,15 @@ public class ExComponent04 {
         panel.setBackground(new Color(240, 240, 240));
         frame.add(panel);
 
+//        ButtonGroup buttonGroup = new ButtonGroup();
+
         JRadioButton obj1 = new JRadioButton();
         JRadioButton obj2 = new JRadioButton("고양이");
         JRadioButton obj3 = new JRadioButton("강아지", true);
+
+//        buttonGroup.add(obj1);
+//        buttonGroup.add(obj2);
+//        buttonGroup.add(obj3);
 
         panel.add(obj1);
         panel.add(obj2);
